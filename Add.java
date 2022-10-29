2628f0f3f3d3d0d7ed6d79cc8703931d5addcfd5
Ashwini1510 <30882378+Ashwini1510@users.noreply.github.com>
2022-10-29 17:10:00 -0500

    first file

---
+++ Add.java
@@ -0,0 +1,11 @@
+public class Add {
+ 
+    public static void main(String args[]){
+    
+        float length, width, area;
+        width = 20.2f;
+        length = 100.1f;
+        area = length * width;
+        System.out.println("area =" + area);
+    }
+    }
